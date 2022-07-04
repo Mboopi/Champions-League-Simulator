69840d6212d7cad236a056e545bf60772ab35e3b
Mboopi <38960170+Mboopi@users.noreply.github.com>
2022-07-04 12:27:36 +0200

    Add country flags to overview

--- cl-draw-simulator/src/components/clubs.tsx
+++ cl-draw-simulator/src/components/clubs.tsx
@@ -1,12 +1,28 @@
 import { Col, Row } from 'react-bootstrap';
+import ReactCountryFlag from 'react-country-flag';
 import data_2022 from '../data/data-2022.json';
 
-const renderClubInfo = (data: any, potNumber: any) => {
-  return data.map((entry: any, i: any) => {
+const lookup = require('country-code-lookup');
+
+// type clubEntry = {
+//   club_name: string;
+//   country: string;
+//   pot: number;
+// };
+
+const getCountryCode = (country: string) => {
+  return lookup.byCountry(country).iso2;
+};
+
+const renderClubInfo = (data: Array<object>, potNumber: number) => {
+  return data.map((entry: any, i: number) => {
     if (entry.pot == potNumber) {
       return (
         <Row key={i}>
-          <p style={{ fontSize: 10 }}>{entry.club_name}</p>
+          <p style={{ fontSize: 12 }}>
+            <ReactCountryFlag countryCode={getCountryCode(entry.country)} />{' '}
+            {entry.club_name}
+          </p>
         </Row>
       );
     }
