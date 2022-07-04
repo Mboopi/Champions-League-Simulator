5a76736d9222b77fabbf3ca3ed2bd8b35a3e32a4
Mboopi <38960170+Mboopi@users.noreply.github.com>
2022-07-04 11:44:17 +0200

    Refactor clubs

--- cl-draw-simulator/src/components/clubs.tsx
+++ cl-draw-simulator/src/components/clubs.tsx
@@ -1,60 +1,39 @@
-import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import data_2022 from '../data/data-2022.json';
 
-const getClubOverview = (mode: String) => {
-  const clubData = data_2022[mode as keyof typeof data_2022];
-  let clubOverview = { 1: [''], 2: [''], 3: [''], 4: [''] };
-
-  for (let i = 0; i < clubData.length; i++) {
-    const clubPot = clubData[i].pot;
-    const clubName = clubData[i].club_name;
-    clubOverview[clubPot as unknown as keyof typeof clubOverview].push(
-      clubName
-    );
-  }
-
-  for (let i = 0; i < Object.keys(clubOverview).length; i++) {
-    clubOverview[(i + 1) as unknown as keyof typeof clubOverview].shift();
-  }
-
-  return clubOverview;
-};
-
-const renderClubsFromPot = (clubs: any) => {
-  return clubs.map((club: any, i: any) => {
-    return (
-      <Row key={i}>
-        <p style={{ fontSize: 10 }}>{club}</p>
-      </Row>
-    );
+const renderClubInfo = (data: any, potNumber: any) => {
+  return data.map((entry: any, i: any) => {
+    if (entry.pot == potNumber) {
+      return (
+        <Row key={i}>
+          <p style={{ fontSize: 10 }}>{entry.club_name}</p>
+        </Row>
+      );
+    }
   });
 };
 
 const Clubs = () => {
-  const allClubs = getClubOverview('group_stage');
-  const potNumbers = Object.keys(allClubs);
-  console.log(allClubs);
-  console.log(Object.keys(allClubs));
+  const drawMode = 'group_stage';
 
   return (
     <>
       <Row>
         <Col>
           <b>Pot 1</b>
-          {renderClubsFromPot(allClubs[1])}
+          {renderClubInfo(data_2022[`${drawMode}`], 1)}
         </Col>
         <Col>
           <b>Pot 2</b>
-          {renderClubsFromPot(allClubs[2])}
+          {renderClubInfo(data_2022[`${drawMode}`], 2)}
         </Col>
         <Col>
           <b>Pot 3</b>
-          {renderClubsFromPot(allClubs[3])}
+          {renderClubInfo(data_2022[`${drawMode}`], 3)}
         </Col>
         <Col>
           <b>Pot 4</b>
-          {renderClubsFromPot(allClubs[4])}
+          {renderClubInfo(data_2022[`${drawMode}`], 4)}
         </Col>
       </Row>
     </>
