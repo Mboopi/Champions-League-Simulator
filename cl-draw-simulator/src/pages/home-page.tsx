dcaaa693c8e94b177d38cd1b742232a900d90a33
Mboopi <38960170+Mboopi@users.noreply.github.com>
2022-08-07 19:26:50 +0200

    Added information about the site on the main page

--- cl-draw-simulator/src/pages/home-page.tsx
+++ cl-draw-simulator/src/pages/home-page.tsx
@@ -2,14 +2,52 @@ import { useState } from 'react';
 import data_2022 from '../data/data-2022.json';
 import GlobalStyle from '../styling/global-style';
 import OverviewCard from '../components/overview-card';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import SimulationOverview from '../components/simulation';
 import ClubsOverview from '../components/clubs';
+
+const style = {
+  infoContainer: {
+    textAlign: 'center' as 'center',
+    marginTop: -30,
+    marginBottom: 20,
+  },
+};
 
 const HomePage = () => {
   // const [drawMode, setDrawMode] = useState('group_stage');
   const drawMode = 'group_stage';
   const [clubData, setClubData] = useState(data_2022[`${drawMode}`]);
+  const [showInfo, setShowInfo] = useState(false);
+
+  const renderInfo = () => {
+    if (showInfo) {
+      return (
+        <div style={style.infoContainer}>
+          <p>
+            This site allows you to simulate the draw of the group stage of the
+            UEFA Champions League football tournament of the season 2022/23.
+            This website is not associated with UEFA in any way. It is simply a
+            personal project for fun.{' '}
+            <a onClick={() => setShowInfo(false)} style={{ cursor: 'pointer' }}>
+              <u>Click to minimize</u>
+            </a>
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div style={style.infoContainer}>
+          <u
+            onClick={() => setShowInfo(true)}
+            style={{ color: 'white', cursor: 'pointer' }}
+          >
+            More info
+          </u>
+        </div>
+      );
+    }
+  };
 
   return (
     <div style={GlobalStyle.homePage}>
@@ -17,6 +55,8 @@ const HomePage = () => {
       <p style={GlobalStyle.pageSubTitle}>
         UEFA Champions League Draw Simulator
       </p>
+
+      {renderInfo()}
 
       <Row style={{ paddingBottom: 20 }}>
         <Col xs={12} sm={12} md={6} lg={6} style={{ marginBottom: 20 }}>
